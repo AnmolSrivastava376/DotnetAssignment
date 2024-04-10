daa3f680852578fa89a2217922758d688c03b6cd
AnmolSrivastava376 <anmol.srivastva@accolitedigital.com>
2024-04-10 09:34:11 +0530

    first

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DotNetDesignPatterns
+{
+    class Program
+    {
+        private static int Add(int a,int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The sum is : "+Add(a,b));
+            Console.ReadLine();
+        }
+    }
+}
